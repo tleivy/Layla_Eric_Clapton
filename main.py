e428993819abec8ca3aa83fab4157394826bd056
to-mer <tomerleivy1@gmail.com>
2022-08-18 12:03:19 +0300

    feat: verse2 #2

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ Vers 1 - David
 
 Vers 2 - Tomer
 
+Layla, you've got me on my knees
+Layla, I'm begging, darling please
+Layla, darling won't you ease my worried mind
+
 Vers 3 - Lavian
 
 Vers 4 - Roi
